0685bb33fcb2985c32700cac1d8eee1ecc57b877
Yoimar <urbina.yoimar@gmail.com>
2025-05-17 22:44:52 -0400

    Remove test files

--- src/utils/test-config.ts
+++
@@ -1,8 +0,0 @@
-import { Game } from '@/types/games';
-import { useGames } from '@/hooks/useGames';
-import { ThemedText } from '@/components/ThemedText';
-
-// This is just a test file to verify TypeScript paths
-export const testConfig = {
-  isWorking: true
-}; 
